43d60963994c24701f998d0b3858f2fc1c586a20
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2022-12-15 20:22:35 +0530

    Add files via upload

---
+++ day 29.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char i[10],sum[10];
+    cout<<"enter any string = ";
+    cin>>i[];
+    cout<<"enter any string = ";
+    cin>>sum[];
+    cout<<"concatinated = "<<i[10]+sum[10];
+    return 0;
+}
